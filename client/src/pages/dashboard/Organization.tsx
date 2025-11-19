90fce7b251f9fa9df47c234615e0456c02091969
Jeffrey Lau <jeffrey.lau@foundit.property>
2025-11-19 16:08:26 +1100

    Update Organization.tsx

--- client/src/pages/dashboard/Organization.tsx
+++ client/src/pages/dashboard/Organization.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '@/context/AuthContext';
 import { RESOURCE_CONFIG } from '@/routes/resources';
 import { useEffect, useState } from 'react';
 import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 const Organization = (): JSX.Element => {
   const { isAuthenticated, loading, organization, user } = useAuth();
